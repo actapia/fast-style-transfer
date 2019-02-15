61fa97e39d0cbcf41e012de29e83afded63a3b6e
Weilian Song <weiliansong19@gmail.com>
2019-02-15 02:59:55 -0500

    Updated eval scripts for single image inference

--- evaluate.py
+++ evaluate.py
@@ -1,207 +1,124 @@
 from __future__ import print_function
-import cv2
+
+# import cv2
 import sys
 sys.path.insert(0, 'src')
-import transform, numpy as np, vgg, pdb, os
+
+import transform
 import scipy.misc
+import numpy as np
 import tensorflow as tf
-from utils import save_img, get_img, exists, list_files
-from argparse import ArgumentParser
+from utils import save_img, get_img
+from skimage.transform import resize
+import matplotlib.pyplot as plt
 
-BATCH_SIZE = 4
-DEVICE = '/gpu:0'
+def transfer(img, style_ckpt):
+  g = tf.Graph()
+  config = tf.ConfigProto(allow_soft_placement=True)
+  config.gpu_options.allow_growth = True
 
-# get img_shape
-def ffwd(data_in, paths_out, checkpoint_dir, device_t='/gpu:0', batch_size=4):
-    assert len(paths_out) > 0
-    is_paths = type(data_in[0]) == str
-    if is_paths:
-        assert len(data_in) == len(paths_out)
-        img_shape = get_img(data_in[0]).shape
+  with g.as_default(), g.device('/gpu:0'), tf.Session(config=config) as sess:
+
+    # Resize image
+    h, w, _ = img.shape
+    mx, my = h/2, w/2
+
+    if float(w) / float(h) < 2:
+      h = w/2
+      img = img[mx-h/2:mx+h/2, :, :]
+      img = resize(img, (400, 800))
+
     else:
-        assert data_in.size[0] == len(paths_out)
-        img_shape = X[0].shape
+      w = 2 * h
+      img = img[:, my-w/2:my+w/2, :]
+      img = resize(img, (400, 800))
 
-    g = tf.Graph()
-    batch_size = min(len(paths_out), batch_size)
-    curr_num = 0
-    soft_config = tf.ConfigProto(allow_soft_placement=True)
-    soft_config.gpu_options.allow_growth = True
-    with g.as_default(), g.device(device_t), \
-            tf.Session(config=soft_config) as sess:
-        batch_shape = (batch_size,) + img_shape
-        img_placeholder = tf.placeholder(tf.float32, shape=batch_shape,
-                                         name='img_placeholder')
+    # Build inference network
+    img = np.reshape(img, (1,) + img.shape)
+    batch_shape = img.shape
+    img_holder = tf.placeholder(tf.float32, shape=batch_shape)
+    styled_img = transform.net(img_holder)
 
-        preds = transform.net(img_placeholder)
-        saver = tf.train.Saver()
-        if os.path.isdir(checkpoint_dir):
-            ckpt = tf.train.get_checkpoint_state(checkpoint_dir)
-            if ckpt and ckpt.model_checkpoint_path:
-                saver.restore(sess, ckpt.model_checkpoint_path)
-            else:
-                raise Exception("No checkpoint found...")
-        else:
-            saver.restore(sess, checkpoint_dir)
+    # Restore style weights
+    saver = tf.train.Saver()
+    saver.restore(sess, style_ckpt)
 
-        num_iters = int(len(paths_out)/batch_size)
-        for i in range(num_iters):
-            pos = i * batch_size
-            curr_batch_out = paths_out[pos:pos+batch_size]
-            if is_paths:
-                curr_batch_in = data_in[pos:pos+batch_size]
-                X = np.zeros(batch_shape, dtype=np.float32)
-                for j, path_in in enumerate(curr_batch_in):
-                    img = get_img(path_in)
-                    assert img.shape == img_shape, \
-                        'Images have different dimensions. ' +  \
-                        'Resize images or use --allow-different-dimensions.'
-                    X[j] = img
-            else:
-                X = data_in[pos:pos+batch_size]
+    styled_img_ = sess.run(styled_img, feed_dict={img_holder: img})
 
-            _preds = sess.run(preds, feed_dict={img_placeholder:X})
-            for j, path_out in enumerate(curr_batch_out):
-                save_img(path_out, _preds[j])
+  return styled_img_
 
-        remaining_in = data_in[num_iters*batch_size:]
-        remaining_out = paths_out[num_iters*batch_size:]
-    if len(remaining_in) > 0:
-        ffwd(remaining_in, remaining_out, checkpoint_dir,
-            device_t=device_t, batch_size=1)
-
-def ffwd_to_img(in_path, out_path, checkpoint_dir, device='/cpu:0'):
-    paths_in, paths_out = [in_path], [out_path]
-    ffwd(paths_in, paths_out, checkpoint_dir, batch_size=1, device_t=device)
-
-def build_parser():
-    parser = ArgumentParser()
-    parser.add_argument('--checkpoint', type=str,
-                        dest='checkpoint_dir',
-                        help='dir or .ckpt file to load checkpoint from',
-                        metavar='CHECKPOINT', required=True)
-
-    parser.add_argument('--in-path', type=str,
-                        dest='in_path',help='dir or file to transform',
-                        metavar='IN_PATH', required=True)
-
-    help_out = 'destination (dir or file) of transformed file or files'
-    parser.add_argument('--out-path', type=str,
-                        dest='out_path', help=help_out, metavar='OUT_PATH',
-                        required=True)
-
-    parser.add_argument('--device', type=str,
-                        dest='device',help='device to perform compute on',
-                        metavar='DEVICE', default=DEVICE)
-
-    parser.add_argument('--batch-size', type=int,
-                        dest='batch_size',help='batch size for feedforwarding',
-                        metavar='BATCH_SIZE', default=BATCH_SIZE)
-
-    parser.add_argument('--allow-different-dimensions', action='store_true',
-                        dest='allow_different_dimensions',
-                        help='allow different image dimensions')
-
-    return parser
-
-def check_opts(opts):
-    exists(opts.checkpoint_dir, 'Checkpoint not found!')
-    exists(opts.in_path, 'In path not found!')
-    if os.path.isdir(opts.out_path):
-        exists(opts.out_path, 'out dir not found!')
-        assert opts.batch_size > 0
-
-def main():
-    parser = build_parser()
-    opts = parser.parse_args()
-    check_opts(opts)
-
-    if not os.path.isdir(opts.in_path):
-        if os.path.exists(opts.out_path) and os.path.isdir(opts.out_path):
-            out_path = \
-                    os.path.join(opts.out_path,os.path.basename(opts.in_path))
-        else:
-            out_path = opts.out_path
-
-        ffwd_to_img(opts.in_path, out_path, opts.checkpoint_dir,
-                    device=opts.device)
-    else:
-        files = list_files(opts.in_path)
-        full_in = [os.path.join(opts.in_path,x) for x in files]
-        full_out = [os.path.join(opts.out_path,x) for x in files]
-
-        if opts.allow_different_dimensions:
-            ffwd_different_dimensions(full_in, full_out, opts.checkpoint_dir,
-                    device_t=opts.device, batch_size=opts.batch_size)
-        else :
-            ffwd(full_in, full_out, opts.checkpoint_dir, device_t=opts.device,
-                    batch_size=opts.batch_size)
-
-# Decide whether the user wants examples or take their own
-#   If examples, then display some options
-#   If take your own, then activate webcam and take a picture
-#
-# Then display some options for what style to transfer
-#   After each image, have the option of saving the image for email later.
-#   Also have the option to do a different style
+# After each image, have the option of saving the image for email later.
+# Also have the option to do a different style
 #
 # After the person leaves, then have the option of resetting
 if __name__ == '__main__':
-    print('Source image:')
-    print('  [1] Choose from examples')
-    print('  [2] Take your own!')
-    source = int(input('Selection: '))
+  print('Source image:')
+  print('  [1] Choose from examples')
+  print('  [2] Take your own!')
+  source = int(input('Selection: '))
 
-    if source == 1:
-      print('Choose from examples:')
-      print('  [1] Chicago')
-      print('  [2] Modern Building')
-      example_idx = int(input('Selection: '))
+  if source == 1:
+    print('\nChoose from examples:')
+    print('  [1] Chicago')
+    print('  [2] Modern Building')
+    example_idx = int(input('Selection: '))
 
-      example_dict = {
-        1: 'examples/content/chicago.jpg',
-        2: 'examples/content/stata.jpg',
-      }
-      img_fname = example_dict[example_idx]
-
-    elif source == 2:
-      print('Taking a picture with the webcam')
-
-      cam = cv2.VideoCapture(0)
-      cv2.namedWindow('camera')
-
-      while True:
-        print('test')
-        ret, frame = cam.read()
-        cv2.imshow('camera', frame)
-        if not ret:
-          break
-        k = cv2.waitKey(1)
-
-        # Space is pressed
-        if k % 256 == 32:
-          img_fname = 'examples/user_image.png'
-          cv2.imwrite(img_name, frame)
-          print('Picture taken!')
-          break
-
-        cam.release()
-        cv2.destroyAllWindows()
-
-    print('\nSource style')
-    print('  [1] La Muse')
-    print('  [2] Rain Princess')
-    print('  [3] The Scream')
-    print('  [4] The Shipwreck of the Minotaur')
-    print('  [5] Udnie')
-    print('  [6] Wave')
-    style_idx = int(input('Selection: '))
-
-    style_dict = {
-      1: 'ckpts/la_muse.ckpt',
-      2: 'ckpts/rain_princess.ckpt',
-      3: 'ckpts/scream.ckpt',
-      4: 'ckpts/wreck.ckpt',
-      5: 'ckpts/udnie.ckpt',
-      6: 'ckpts/wave.ckpt',
+    example_dict = {
+      1: 'examples/content/chicago.jpg',
+      2: 'examples/content/stata.jpg',
+      3: 'examples/content/uk.jpg',
+      4: 'examples/content/basketball.jpg',
     }
+    img_fname = example_dict[example_idx]
+    img = get_img(img_fname)
+
+  elif source == 2:
+    print('\nTaking a picture with the webcam')
+
+    cam = cv2.VideoCapture(0)
+    cv2.namedWindow('camera')
+
+    while True:
+      print('test')
+      ret, frame = cam.read()
+      cv2.imshow('camera', frame)
+      if not ret:
+        break
+      k = cv2.waitKey(1)
+
+      # Space is pressed
+      if k % 256 == 32:
+        img_fname = 'examples/user_image.png'
+        cv2.imwrite(img_name, frame)
+        print('Picture taken!')
+        break
+
+      cam.release()
+      cv2.destroyAllWindows()
+
+  print('\nSource style')
+  print('  [1] La Muse')
+  print('  [2] Rain Princess')
+  print('  [3] The Scream')
+  print('  [4] The Shipwreck of the Minotaur')
+  print('  [5] Udnie')
+  print('  [6] Wave')
+  style_idx = int(input('Selection: '))
+
+  style_dict = {
+    1: 'ckpts/la_muse.ckpt',
+    2: 'ckpts/rain_princess.ckpt',
+    3: 'ckpts/scream.ckpt',
+    4: 'ckpts/wreck.ckpt',
+    5: 'ckpts/udnie.ckpt',
+    6: 'ckpts/wave.ckpt',
+  }
+  style_ckpt = style_dict[style_idx]
+
+  styled_img = transfer(img, style_ckpt)[0]
+  styled_img = np.clip(styled_img, 0, 255).astype(np.uint8)
+
+  plt.imshow(styled_img)
+  plt.axis('off')
+  plt.tight_layout()
+  plt.show()
